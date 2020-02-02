83f24f6b4d8a06fc8bcb4749424137fa593fc096
Ruby Iris Juric <ruby@srxl.me>
2020-02-03 00:38:23 +1100

    SUBMITME: iio: accel: bma180: BMA253 support

--- drivers/iio/accel/bma180.c
+++ drivers/iio/accel/bma180.c
@@ -1181,6 +1181,10 @@ static const struct of_device_id bma180_of_match[] = {
 		.data = (void *)BMA250
 	},
 	{
+		.compatible = "bosch,bma253",
+		.data = (void *)BMA254
+	},
+	{
 		.compatible = "bosch,bma254",
 		.data = (void *)BMA254
 	},
